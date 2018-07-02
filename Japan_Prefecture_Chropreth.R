2b5243a4be6cabb563af123ab518834af4b1c197
Chisato <chisatini@gmail.com>
2018-07-01 18:38:52 -0700

    Trying to figure out how to use geojson file to create map with ggplot2.

--- Japan_Prefecture_Chropreth.R
+++ Japan_Prefecture_Chropreth.R
@@ -3,17 +3,29 @@
 
 ## Japan Prefecture
 library(rvest)
+library(janitor)
 
+## scrape data from Wikipedia Page
 japan.pref <- read_html("https://en.wikipedia.org/wiki/Prefectures_of_Japan") %>%
   html_nodes(".wikitable") %>% html_table(fill=T)
 japan.pref <-japan.pref[[2]]
+
+japan.pref <- japan.pref %>% 
+  mutate(id = as.integer(str_remove(str_extract(ISO,"\\d+"),"^0"))) %>%
+  clean_names()
 
 library(geojsonio)
 japan.map <- geojson_read("https://raw.githubusercontent.com/chichacha/land/master/japan.geojson", what = "sp")
 
 library(broom)
-japan.map.fortify <- japan.map %>% tidy(region = "id") %>% mutate(id=as.integer(id))
-japan.map.data <- japan.map@data %>% arrange(id)
+
+## Out of tidy is always a data.frame with disposable row names...  
+japan.map.fortify <- japan.map %>% 
+  tidy(region = "id") %>% 
+  mutate(id=as.integer(id)) %>%
+  left_join(japan.pref %>% select(id, region))
+japan.map.data <- japan.map@data %>% arrange(id) %>%
+  left_join(japan.pref %>% select(id, prefecture, kanji, capital, region, iso))
 
 
 japan.map.fortify %>% 
@@ -26,3 +38,13 @@ japan.map.fortify %>%
                     name="Prefecture")
 
 ggsave(filename="images/JapanPrefectureColouring.png", width=16, height=12)
+
+japan.map.fortify %>% 
+  ggplot(aes(x=long, y=lat, group=group)) +
+  geom_polygon(aes(fill=fct_inorder(region)), color="#00000080") +
+  theme_void(base_family="Roboto Condensed") +
+  coord_map() +
+  scale_fill_brewer(name="8 Regions in Japan", palette="Set2")
+
+ggsave(filename="images/JapanRegions.png", width=16, height=9)
+
